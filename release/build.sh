0b8c212cd246ccf673a39923dc3a9ad520d818fa
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2018-12-11 17:25:53 -0800

    Find the titus-log-server for the region/account from EDDA

--- release/build.sh
+++ release/build.sh
@@ -5,5 +5,6 @@ cp -r pkg titus-agent-netconsole-0.${BUILD_NUMBER}
 cd /src/titus-agent-netconsole-0.${BUILD_NUMBER}
 
 dh_make --yes --indep --createorig
-mv /src/pkg/install debian/install
+cp /src/pkg/install debian/install
+cp /src/pkg/postinst debian/postinst
 debuild -us -uc
