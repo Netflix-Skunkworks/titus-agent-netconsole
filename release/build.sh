4e54434bb10db02c1d46574d8c5d66ba9b5e2763
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2018-12-12 11:29:26 -0800

    Add debugging logs to build.sh

--- release/build.sh
+++ release/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 cd /src
+
+echo current directory:
+pwd
+
+echo current directory contents:
+ls -la
+
+echo TITUS_NETCONSOLE_VERSION: $TITUS_NETCONSOLE_VERSION
 cp -r pkg titus-agent-netconsole-${TITUS_NETCONSOLE_VERSION}
 cd /src/titus-agent-netconsole-${TITUS_NETCONSOLE_VERSION}
 
